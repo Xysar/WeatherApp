58d05f57d344f91cbfda6fa3f184c6e9d93c5862
Cesar Camacho <cesarcamacho760@gmail.com>
2022-10-28 14:38:11 -0700

    Add search function and icon change

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let currentCity = "Vista";
+
 function getLocationInfo(cityName) {
   return fetch(
     `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=a9474bb6a4b22a8da4d1ff6d172ff945`,
@@ -13,7 +15,7 @@ function getLocationCoord(cityName) {
   });
 }
 
-async function getLocationWeatherData(latitude, longitude) {
+async function getCurrentWeatherData(latitude, longitude) {
   let result = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=a9474bb6a4b22a8da4d1ff6d172ff945`,
     { mode: "cors" }
@@ -22,34 +24,131 @@ async function getLocationWeatherData(latitude, longitude) {
   return resultjson;
 }
 
-async function getLocationWeather(cityName) {
+async function getCurrentWeather(cityName) {
   let locationCoord = await getLocationCoord(cityName);
-  let weather = await getLocationWeatherData(
-    locationCoord[0],
-    locationCoord[1]
-  );
+  let weather = await getCurrentWeatherData(locationCoord[0], locationCoord[1]);
   return weather;
 }
 
-let currentCity = "Vista";
+// async function getDailyForecast(cityName) {
+//   let locationCoord = await getLocationCoord(cityName);
+//   console.log(locationCoord);
+//   let result = await fetch(
+//     `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${locationCoord[0]}&lon=${locationCoord[1]}&cnt=4&appid=a9474bb6a4b22a8da4d1ff6d172ff945`,
+//     { mode: "cors" }
+//   );
+//   return result;
+// }
+
+function convertKelvinToFahrenheit(temp) {
+  return Math.round((temp - 273.15) * (9 / 5) + 32);
+}
+
+async function updateInfo(cityName) {
+  currentCity = cityName;
+  let cityInfo = await getCurrentWeather(cityName);
+  let cityDisplay = document.querySelector(".city-name");
+  let temperatureDisplay = document.querySelector(".today-temp");
+  let starterCity = getLocationInfo(cityName);
+  starterCity.then((e) => {
+    cityDisplay.innerText = e[0].name + ", " + e[0].state + ", " + e[0].country;
+  });
+
+  let startingTemp = getCurrentWeather(currentCity);
+  startingTemp.then((e) => {
+    temperatureDisplay.innerText =
+      convertKelvinToFahrenheit(Number(e.main.temp)) + String.fromCharCode(176);
+  });
+
+  updateIcon(cityInfo);
+}
+
+function updateIcon(cityInfo) {
+  let todayIcon = document.querySelector(".today-icon");
+  todayIcon.src = `http://openweathermap.org/img/wn/${cityInfo.weather[0].icon}@2x.png`;
+}
+
+function updateTemperatures() {}
+
+function updateCityHeader() {}
 
 const initUI = () => {
   const container = document.getElementById("container");
-  let cityHeader = document.createElement("h1");
-  cityHeader.classList.add("cityname");
 
-  let starterCity = getLocationInfo(currentCity);
-  starterCity.then((e) => {
-    cityHeader.innerText = e[0].name + e[0].state + e[0].country;
-  });
-  container.appendChild(cityHeader);
+  const header = document.createElement("div");
+  header.classList.add("header");
+  container.appendChild(header);
+
+  let cityForm = createCityForm();
+  header.appendChild(cityForm);
+
+  const displayArea = document.createElement("div");
+  displayArea.classList.add("display-area");
+
+  let cityHeader = document.createElement("h1");
+  cityHeader.classList.add("city-name");
+
+  updateInfo(currentCity);
+
+  displayArea.appendChild(cityHeader);
+
+  const dailyForecast = document.createElement("div");
+  dailyForecast.classList.add("daily-forecast");
+
+  const todayTempDisplay = document.createElement("div");
+  todayTempDisplay.classList.add("today-temp-display");
+
   const cityTemperature = document.createElement("h1");
-  cityTemperature.classList.add("citytemp");
-  let startingTemp = getLocationWeather(currentCity);
-  startingTemp.then((e) => {
-    cityTemperature.innerText = e.main.temp;
-  });
-  container.appendChild(cityTemperature);
+  cityTemperature.classList.add("today-temp");
+
+  const todayWeatherIcon = document.createElement("img");
+  todayWeatherIcon.classList.add("today-icon");
+
+  todayTempDisplay.appendChild(todayWeatherIcon);
+  todayTempDisplay.appendChild(cityTemperature);
+
+  dailyForecast.appendChild(todayTempDisplay);
+
+  displayArea.appendChild(dailyForecast);
+  container.appendChild(displayArea);
 };
+
+function createDailyTempDisplay(num) {
+  let tempDisplay = document.createElement("div");
+  tempDisplay.classList.add("daily-temp");
+
+  let temperature = document.createElement("p");
+  temperature.classList.add("daily-temp-display");
+  let icon = document.createElement("img");
+  icon.classList.add("daily-icon");
+  tempDisplay.setAttribute("data", num);
+  return tempDisplay;
+}
+
+function createCityForm() {
+  let form = document.createElement("form");
+  form.classList.add("city-form");
+  form.setAttribute("method", "post");
+  form.setAttribute("action", "");
+
+  let cityInput = document.createElement("input");
+  cityInput.classList.add("city-input");
+
+  let citySubmit = document.createElement("button");
+  citySubmit.classList.add("city-submit");
+  citySubmit.innerText = "Search";
+
+  citySubmit.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (cityInput.value === "") {
+      return;
+    }
+    updateInfo(cityInput.value);
+    cityInput.value = "";
+  });
+  form.appendChild(cityInput);
+  form.appendChild(citySubmit);
+  return form;
+}
 
 initUI();
